b1182194b5b304a6c89feef5d107217f417c70b2
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-04-07 22:28:39 -0300

    Add comments and outputs

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
+    // Simple comment
+
+    /* Multi 
+    line
+    comment
+    */
+
     println!("Hello World");
+    print!("Hello World"); // stay in the same line
+
+    println!("My first name is {} and my last name is {}", "Rodrigo", "Silva");
 }
