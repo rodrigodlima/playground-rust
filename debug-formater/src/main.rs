10a6745210120f0c0b486f1348b78bb93d8edb8c
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2026-05-02 17:33:39 -0300

    Implement Display trait for Ponto struct and update main function

--- debug-formater/src/main.rs
+++ debug-formater/src/main.rs
@@ -3,3 +3,13 @@ fn main() {
     println!("Debug: {:?}", v);
     //println!("Normal: {}", v); Here, it will gererate a compilation error, because Vec<T> and HashMap only implement Debug
 }
+
+use std::fmt;
+
+struct Ponto { x: i32, y: i32 }
+
+impl fmt::Display for Ponto {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
